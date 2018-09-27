2b512734583990452562911aa900b32fbde4f48e
Jakub Mišina <jakub.misina@gmail.com>
2018-09-27 17:50:52 +0200

    First working version with main screen and counter increase.

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
 require 'sinatra'
+require 'redis'
+
+redis = Redis.new
 
 get '/' do
-  'Hello, world!'
+  "<h1>Hello, world!</h2>Actual counter: #{redis.get 'frank-counter'}"
+end
+
+get '/counter' do
+  counter = redis.incr 'frank-counter'
+  counter.to_s
 end
